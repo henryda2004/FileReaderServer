f9e6b9f25a75cfa2a7ef9c3cce2b789d40c00c10
henryda2004 <henryda2004@gmail.com>
2022-10-23 22:46:50 -0600

    Lectura de los 3 tipos de archivos
    Implementacion de bibliotecas para la lectura de pdfs y docx

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,58 @@
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.openxml4j.util.ZipEntrySource;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
 
-    public void ReadPDFile() throws Exception {
+
+    public static void main(String[] args) throws Exception {
+        //File file = new File("/Users/henry/Downloads/pdfexample.pdf");
+
+        //pal docx
+        try{
+            XWPFDocument fis = new XWPFDocument(new FileInputStream("C:/Users/henry/Downloads/HenryNúñez_Tarea 1.docx"));
+            XWPFWordExtractor we = new XWPFWordExtractor(fis);
+            System.out.println(we.getText());
+        } catch (Exception e){
+            System.out.println(e);
+        }
 
 
-        File file = new File("/Users/henry/Downloads/txtexample.txt");
-        //FileInputStream fis = new FileInputStream(file);
-        //PDDocument pdfdocument = PDDocument.load(fis);
-    }
 
 
-
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("/Users/henry/Downloads/pdfexample.pdf");
-
-
+        /*
         //pal pedeefe
+        FileInputStream fis = new FileInputStream(file);
+
+        PDDocument pdfdocument = PDDocument.load(fis);
+        System.out.println(pdfdocument.getPages().getCount());
+        PDFTextStripper pdfTextStripper = new PDFTextStripper();
+        String docText = pdfTextStripper.getText(pdfdocument);
+        System.out.println(docText);
+
+        pdfdocument.close();
+        fis.close();
 
 
 
 
-
-        //pal tequiste
+        pal tequiste
         Scanner scan = new Scanner(file);
         System.out.println(scan.next());
         while(scan.hasNextLine()){
-            System.out.println(scan.nextLine());
+            //System.out.println(scan.nextLine());
         }
-
+        */
 
     }
 
